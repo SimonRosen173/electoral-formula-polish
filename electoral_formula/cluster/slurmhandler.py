4ef0636c373db2b72a285b1796c60ad68d53e9e4
Simon Rosen <simonrosen173@gmail.com>
2024-04-09 13:18:35 +0200

    :/

--- electoral_formula/cluster/slurmhandler.py
+++ electoral_formula/cluster/slurmhandler.py
@@ -73,10 +73,11 @@ class SlurmHandler:
         ###################
         # FINISH COMMANDS #
         ###################
-        finish_commands = f"python3 {API_PATH} --join {TMP_LOCAL_DATA_FOLDER} --n-batches {n_batches} --formulae amend_orig"
+        # finish_commands = f"python3 {API_PATH} --join {TMP_LOCAL_DATA_FOLDER} --n-batches {n_batches} --formulae amend_orig"
 
         # zip files
-        finish_commands += "\n" + f"zip -r {TMP_LOCAL_FOLDER}/data.zip {TMP_LOCAL_DATA_FOLDER}/agr/*"
+        # finish_commands += "\n" + f"zip -r {TMP_LOCAL_FOLDER}/data.zip {TMP_LOCAL_DATA_FOLDER}/agr/*"
+        finish_commands = f"zip -r {TMP_LOCAL_FOLDER}/data.zip {TMP_LOCAL_DATA_FOLDER}/*"
         # finish_commands += "\n" + f"zip -r {TMP_LOCAL_FOLDER}/data.zip {TMP_LOCAL_DATA_FOLDER}/*"
         finish_commands += "\n" + f"mv {TMP_LOCAL_FOLDER}/data.zip {self.cluster_data_path}/data.zip"
         slurm_str = slurm_str.replace("{finish_commands}", finish_commands)
